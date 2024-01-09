00e848096882c929f17ec2eab94c9f3cc9f6df90
amal ibrahim <93553468+Amal-AbdelSalam56@users.noreply.github.com>
2024-01-09 18:09:09 +0200

    prevent post

--- src/components/ui/createPost/CreatePost.jsx
+++ src/components/ui/createPost/CreatePost.jsx
@@ -30,12 +30,21 @@ export default function CreatePost({ placeholder, buttons, isNormalPost }) {
   const { user, error, msg, token } = useSelector((state) => state.auth);
   const URL = import.meta.env.VITE_REACT_APP_API_KEY;
 
+  const uploads = useSelector((state) => state.progress.uploads);
+
   function closeModal() {
     setIsOpen(false);
   }
 
   function openModal() {
-    setIsOpen(true);
+    // Check if there is any ongoing upload
+    const isUploading = uploads.some((upload) => upload.isLoading);
+
+    // Prevent opening the modal if there is an upload in progress
+    if (!isUploading) {
+      setIsOpen(true);
+      setTitle("Post Text");
+    }
   }
 
   return (
